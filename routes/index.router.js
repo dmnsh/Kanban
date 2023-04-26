ad5c82881b975a8a43cec31b5126bed20503914f
Dmtrii Manushin <d.manushin@rpg-gruppe.de>
2023-04-26 14:54:43 +0200

    remove user system; it is not neccessary now

--- routes/index.router.js
+++ routes/index.router.js
@@ -2,7 +2,6 @@ const { Router } = require('express')
 const router = Router()
 
 router.use('/workspace', require('./workspace.router'))
-router.use('/user', require('./user.router'))
 router.use('/board', require('./board.router'))
 router.use('/panel', require('./panel.router'))
 router.use('/task', require('./task.router'))
